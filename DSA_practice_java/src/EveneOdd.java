dd141177b809d72c2d3ba8a8beecbefd1515ec4b
Devanand Farkade <115337013+Devanandfarkade@users.noreply.github.com>
2025-05-16 10:42:16 +0530

    Add files via upload
    initial commit

---
+++ DSA_practice_java/src/EveneOdd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class EveneOdd {
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of A = > ");
+        int a=sc.nextInt();
+        System.out.println("Enter the value of B = > ");
+        int b=sc.nextInt();
+       if (a==b) {
+           System.out.println("A ia equals to B");
+       }else {
+        if (a<b) {
+            System.out.println("A is lesser then B ");
+        }else {
+            System.out.println("A is greater then B");
+        }
+       } 
+    }
+
+
+}
